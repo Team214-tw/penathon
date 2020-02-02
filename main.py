e642a0c45c28acd4f0be4561663541a12784de00
kaiiiz <at881005@gmail.com>
2020-02-03 02:19:28 +0800

    restruct code & mark issues

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import ast
-import _ast
 import copy
 import uuid
 import astor
@@ -46,104 +45,109 @@ def get_magic(op):
     }
     return magics[type(op).__name__]
 
+
 def get_func_name(ctx, func):
     if 'attr' in dir(func):
         return f"{get_func_name(ctx, func.value)}.{func.attr}"
-    elif isinstance(func, _ast.Name):
-        return infer(ctx, func)
+    elif isinstance(func, ast.Name):
+        return func.id
     else:
         return type(func).__name__.lower()
 
 
 def infer(ctx, e):
-    if isinstance(e, type):
-        return e
+    # if isinstance(e, type):
+    #     return e
 
-    elif isinstance(e, ast.Constant):
-        return type(e.value).__name__
-
-    elif isinstance(e, ast.BoolOp):
-        left_type = infer(ctx, e.left)
-        right_type = infer(ctx, e.right)
-        func_name = get_magic(e.op)
-        typer = Typer()
-        try:
-            funcType = typer.get_type(f"{left_type}.{func_name}")
-        except KeyError:
-            raise Exception(f"{left_type} object has no method {func_name}")
-
-        argList = (right_type,)
-        resultType = TypeVariable()
-        unify(Function(argList, resultType), funcType)
-        if resultType.instance:
-            return resultType.instance
-        else:
-            return resultType
-
-    elif isinstance(e, ast.BinOp):
-        left_type = infer(ctx, e.left)
-        right_type = infer(ctx, e.right)
-        func_name = get_magic(e.op)
-        typer = Typer()
-        try:
-            funcType = typer.get_type(f"{left_type}.{func_name}")
-        except KeyError:
-            raise Exception(f"{left_type} object has no method {func_name}")
-
-        argList = (right_type,)
-        resultType = TypeVariable()
-        unify(Function(argList, resultType), funcType)
-        if resultType.instance:
-            return resultType.instance
-        else:
-            return resultType
-
-    elif isinstance(e, ast.Name):
+    if isinstance(e, ast.Name):
         if e.id in ctx.env:
             return ctx.env[e.id]
         else:
-            typer = Typer()
-            try:
-                funcType = typer.get_type(e.id)
-                return funcType
-            except KeyError:
-                raise Exception(f"Unbound var {e.id}")
+            raise Exception(f"Unbound var {e.id}")
+            # typer = Typer()
+            # try:
+            #     funcType = typer.get_type(e.id)
+            #     return funcType
+            # except KeyError:
+            #     raise Exception(f"Unbound var {e.id}")
+
+    # Base Case
+    elif isinstance(e, ast.List):
+        return 'list'  # undone
+
+    elif isinstance(e, ast.Dict):
+        return 'dict'  # undone
+
+    elif isinstance(e, ast.Set):
+        return 'set'  # undone
+
+    elif isinstance(e, ast.Tuple):
+        return 'tuple'  # undone
+
+    elif isinstance(e, ast.Attribute):
+        return 'attribute'  # undone
+
+    elif isinstance(e, ast.Constant):
+        return type(e.value).__name__  # undone
+
+    # Top-level
+    elif isinstance(e, ast.Expr):
+        return infer(ctx, e.value)
+
+    elif isinstance(e, ast.Assign):
+        valueType = infer(ctx, e.value)
+        for t in e.targets:
+            ctx.env[t.id] = valueType
+        return valueType
 
     elif isinstance(e, ast.FunctionDef):
         newCtx = copy.deepcopy(ctx)
+
+        # create type variables for each argument
         argList = list()
         for i in e.args.args:
             argName = i.arg
-            argType = TypeVariable()
-            newCtx.env[argName] = argType
-            argList.append(argType)
+            argTypeVar = TypeVariable()
+            newCtx.env[argName] = argTypeVar
+            argList.append(argTypeVar)
 
+        # infer
         bodyType = infer(newCtx, e.body)
-        inferredType = Function(argList, bodyType)
-        ctx.env[e.name] = inferredType
-        for i in range(len(argList)):
-            if isinstance(argList[i].instance, str):
-                e.args.args[i].annotation = ast.Name(argList[i].instance)
-        if isinstance(bodyType, str):
+        inferredType = Function(from_type=argList, to_type=bodyType)
+        ctx.env[e.name] = inferredType  # undone
+
+        # generate type informations
+        for i, argTypeVar in enumerate(argList):
+            if isinstance(argTypeVar.instance, str):  # undone
+                e.args.args[i].annotation = ast.Name(argTypeVar.instance)
+        if isinstance(bodyType, str):  # undone
             e.returns = ast.Name(bodyType)
-                
+
         return inferredType
 
-    elif isinstance(e, list):
-        for i in e:
-            if isinstance(i, ast.Return):
-                return infer(ctx, i.value)
-            else:
-                infer(ctx, i)
+    elif isinstance(e, ast.Import):
+        for i in e.names:
+            asname = i.asname or i.name
+            ctx.env[asname] = i.name  # undone typeseeker?
 
+    elif isinstance(e, ast.ImportFrom):
+        for i in e.names:
+            asname = i.asname or i.name
+            ctx.env[asname] = f"{e.module}.{i.name}"  # undone typeseeker?
+
+    elif isinstance(e, ast.Return):
+        return infer(ctx, e.value)
+
+    # Expression
     elif isinstance(e, ast.Lambda):
         newCtx = copy.deepcopy(ctx)
+
         argList = list()
         for i in e.args.args:
             argName = i.arg
-            argType = TypeVariable()
-            newCtx.env[argName] = argType
-            argList.append(argType)
+            argTypeVar = TypeVariable()
+            newCtx.env[argName] = argTypeVar
+            argList.append(argTypeVar)
 
         bodyType = infer(newCtx, e.body)
         inferredType = Function(argList, bodyType)
@@ -152,10 +156,6 @@ def infer(ctx, e):
     elif isinstance(e, ast.Call):
         func_name = get_func_name(ctx, e.func)
         funcType = infer(ctx, ast.Name(func_name))
-        if (len(e.args)) > 0:
-            argType = infer(ctx, e.args[0])
-        else:
-            argType = ()
         argList = list()
         for i in e.args:
             argType = infer(ctx, i)
@@ -164,41 +164,49 @@ def infer(ctx, e):
         unify(Function(argList, resultType), funcType)
         return resultType
 
-    elif isinstance(e, ast.Assign):
-        ctx.env[e.targets[0].id] = infer(ctx, e.value)
-        return ctx.env[e.targets[0].id]
+    # elif isinstance(e, ast.BinOp):
+    #     left_type = infer(ctx, e.left)
+    #     right_type = infer(ctx, e.right)
+    #     func_name = get_magic(e.op)
+    #     typer = Typer()
+    #     try:
+    #         funcType = typer.get_type(f"{left_type}.{func_name}")
+    #     except KeyError:
+    #         raise Exception(f"{left_type} object has no method {func_name}")
 
-    elif isinstance(e, ast.Expr):
-        return infer(ctx, e.value)
+    #     argList = (right_type,)
+    #     resultType = TypeVariable()
+    #     unify(Function(argList, resultType), funcType)
+    #     if resultType.instance:
+    #         return resultType.instance
+    #     else:
+    #         return resultType
 
-    elif isinstance(e, _ast.Import):
-        for i in e.names:
-            asname = i.asname or i.name
-            ctx.env[asname] = i.name
+    # elif isinstance(e, ast.BoolOp):
+    #     left_type = infer(ctx, e.values[0])
+    #     right_type = infer(ctx, e.values[1])
+    #     func_name = get_magic(e.op)
+    #     typer = Typer()
+    #     try:
+    #         funcType = typer.get_type(f"{left_type}.{func_name}")
+    #     except KeyError:
+    #         raise Exception(f"{left_type} object has no method {func_name}")
 
-    elif isinstance(e, _ast.ImportFrom):
-        for i in e.names:
-            asname = i.asname or i.name
-            ctx.env[asname] = f"{e.module}.{i.name}"
+    #     argList = (right_type,)
+    #     resultType = TypeVariable()
+    #     print(Function(argList, resultType), funcType)
+    #     unify(Function(argList, resultType), funcType)
+    #     if resultType.instance:
+    #         return resultType.instance
+    #     else:
+    #         return resultType
 
-    elif isinstance(e, _ast.List):
-        return 'list'
-
-    elif isinstance(e, _ast.Dict):
-        return 'dict'
-
-    elif isinstance(e, _ast.Set):
-        return 'set'
-
-    elif isinstance(e, _ast.Tuple):
-        return 'tuple'
-
-    elif isinstance(e, _ast.Attribute):
-        return 'attribute'
+    # Union type
+    elif isinstance(e, list):  # undone
+        return infer(ctx, e[0].value)
 
     else:
         pass
-
 
 
 def main():
@@ -211,10 +219,7 @@ def main():
 
     ctx = Context()
     for i in x.body:
-        if isinstance(i, ast.Assign):
-            print(i.lineno, infer(ctx, i))
-        else:
-            print(i.lineno, infer(ctx, i))
+        print(i.lineno, infer(ctx, i))
     print(astor.to_source(x), end="")
 
 
